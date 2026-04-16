04ac759f826aa6023057a979914fa49e05ecd08f
Your Name <you@example.com>
2026-04-17 00:12:10 +0530

    Data Ingestion process Done learn extracting it from local Data Base

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+
 import sys
 
 if __name__ == "__main__":
     logging.info("Starting the app...")
 
 
-    # try:        
-    #     a = 1/0
-    # except Exception as e:
-    #     logging.info("Exception occured")
-    #     logging.info(e,sys)
+    try:       
+        # data_ingestion_config = DataIngestionConfig() 
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        logging.info("Exception occured")
+        raise CustomException(e,sys)
